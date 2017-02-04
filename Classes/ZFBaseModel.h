9a0da949392884443120b85039b5ac4fcdb750b6
fanyizeng@yeah.net <fanyizeng@yeah.net>
2017-02-04 16:07:39 +0800

    创建仓库

---
+++ Classes/ZFBaseModel.h
@@ -0,0 +1,13 @@
+//
+//  ZFBaseModel.h
+//  Pods
+//
+//  Created by 曾凡怡 on 2017/2/4.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZFBaseModel : NSObject
+
+@end
---
+++ Classes/ZFBaseModel.m
@@ -0,0 +1,13 @@
+//
+//  ZFBaseModel.m
+//  Pods
+//
+//  Created by 曾凡怡 on 2017/2/4.
+//
+//
+
+#import "ZFBaseModel.h"
+
+@implementation ZFBaseModel
+
+@end
